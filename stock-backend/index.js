63a6952fec59c19115dc6526cd74723189cb0c02
Faizan Ansari <71093180+faizan1166@users.noreply.github.com>
2023-10-24 18:40:21 +0530

    Update index.js

--- stock-backend/index.js
+++ stock-backend/index.js
@@ -40,7 +40,7 @@ app.get('/api/stock-price/:name', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000`);
 
 })
